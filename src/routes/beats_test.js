0f66ab579688444fac201f1f468a62f9b1900466
Artyom Smirnov <artemie.smirnov@gmail.com>
2016-10-07 13:17:33 +0300

    File upload test

--- src/routes/beats_test.js
+++ src/routes/beats_test.js
@@ -8,12 +8,43 @@ before(app.resolveWhenReady);
 beforeEach(clearDb);
 
 describe('api /beats', function () {
+  let agent, accessToken;
+
+  beforeEach(async function() {
+    agent = supertest(app);
+
+    const signUpReponse = await agent.post('/api/users/signup')
+      .send({
+        email: 'test@gmail.com',
+        password: '123123123',
+        username: 'test'
+      });
+
+    accessToken = signUpReponse.body.access_token;
+  });
+
   describe('POST /files', function () {
-    it('should upload file');
-    it('shuld reject non mp3/waw files');
+    it('should upload file', async function() {
+      const uploadBeatFileResponse = await agent.post('/api/beats/files')
+        .set('Authorization', accessToken)
+        .attach('beatFile', 'src/assets_test/audio.mp3')
+        .send({});
+
+      uploadBeatFileResponse.statusCode.should.be.equal(200);
+      uploadBeatFileResponse.body.should.have.property('file');
+      should(uploadBeatFileResponse.body.file.url).be.String();
+
+      (await agent.get(uploadBeatFileResponse.body.file.url))
+        .statusCode.should.be.equal(200);
+    });
+    it('should reject non mp3/waw files');
+    it('should calculate file duration')
   });
 
 	describe('POST /', function () {
-    
+    it('should reject beats with non-existing genre');
+    it('should reject beats with not-existing fileId');
+    it('should create beat');
+    it('should return file info');
 	});
 });
